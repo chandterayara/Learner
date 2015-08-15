098360d68a0bc322788e729d07066ff9c32a3a7a
lavanyakommana <lavanyak36@gmail.com>
2015-08-15 19:50:58 +0530

    Create Printgivenstring.java

---
+++ Printgivenstring.java
@@ -0,0 +1,33 @@
+import Mypack.*;
+class Printgivenstring
+{
+public static String rowbuilder(int i,String s)
+{
+int l,j;
+char c; 
+String x="";
+String rowstring;
+l=s.length( );
+Letterrows q=new Letterrows();
+for(j=0;j<l;j++)
+{
+c=s.charAt(j);
+rowstring=q.getcharrow(c,i);
+x=x+"     "+rowstring;
+
+}
+return(x);
+}
+public static void main(String args[])
+{
+String r; 
+int i;
+String s=args[0];
+for(i=1;i<9;i++)
+{
+r=rowbuilder(i,s);
+System.out.println(r);
+}
+}
+}
+
