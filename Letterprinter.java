e019184f73be314eccd14126c6ef2e0bd658e412
lavanyakommana <lavanyak36@gmail.com>
2015-08-16 21:03:49 +0530

    Create Letterprinter.java

---
+++ Letterprinter.java
@@ -0,0 +1,18 @@
+class Letterprinter
+{
+public static String getcharrow(char c,int i)
+{
+String p="";
+switch(c)
+{
+case 'c':p=getrowC( c,i );break;
+case 'a':p=getrowA( c,i);break;
+case 'h':p=getrowH(c,i);break;
+case 'n':p=getrowN(c,i);break;
+}
+return(p);
+}
+{
+}
+
+}
