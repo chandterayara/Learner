617bff183d56563acae184942855d64791fbd67f
chandterayara <chandterayara@gmail.com>
2015-08-17 13:30:11 +0530

    Update Letterprinter.java

--- Letterprinter.java
+++ Letterprinter.java
@@ -1,15 +1,19 @@
-class Letterprinter
+abstract class Letterprinter
 {
-public static String getcharrow(char c,int i,Letterprinter printformat)
-{
-String p="";
-switch(c)
-{
-case 'c':p=getrowC( c,i,printformat );break;
-case 'a':p=getrowA( c,i,printformat);break;
-case 'h':p=getrowH(c,i,printformat);break;
-case 'n':p=getrowN(c,i,printformat);break;
-}
-return(p);
-}
+  public static String getcharrow(char c,int i,Letterprinter printformat)
+  {
+    String p="";
+    switch(c)
+    {
+      case 'c':p=getrowC( c,i,printformat );break;
+      case 'a':p=getrowA( c,i,printformat);break;
+      case 'h':p=getrowH(c,i,printformat);break;
+      case 'n':p=getrowN(c,i,printformat);break;
+    }
+    return(p);
+  }
+  abstract String getrowC(char c, int i,LetterPrinter printformat);
+  abstract String getrowA(char c, int i,LetterPrinter printformat);
+  abstract String getrowH(char c, int i,LetterPrinter printformat);
+  abstract String getrowN(char c, int i,LetterPrinter printformat);
 }
