7f83632d4038b5e744cb56f28ee009b4c1c744b5
chandterayara <chandterayara@gmail.com>
2015-08-17 16:47:48 +0530

    Update Printgivenstring.java

--- Printgivenstring.java
+++ Printgivenstring.java
@@ -1,32 +1,33 @@
 import Mypack.*;
 class Printgivenstring
 {
-public static String rowbuilder(int i,String s, Letterprinter printformat)
-{
-int l,j;
-char c; 
-String x="";
-String rowstring;
-l=s.length( );
-Charformat q=new Charformat();
-for(j=0;j<l;j++)
-{
-c=s.charAt(j);
-rowstring=q.getcharrow(c,i,printformat);
-x=x+"     "+rowstring;
-
-}
-return(x);
-}
+  public static String rowbuilder(int i,String s, Letterprinter printformat)
+  {
+    int l,j;
+    char c; 
+    String x="";
+    String rowstring;
+    l=s.length( );
+    for(j=0;j<l;j++)
+    {
+      c=s.charAt(j);
+      rowstring = printformat.getcharrow(c,i,printformat);
+      x=x+"     "+rowstring;
+    }
+    return(x);
+  }
 public static void main(String args[])
 {
 String r; 
 int i;
 String s=args[0];
 LetterPrinter printFormat ; 
-if(args[0]="")
+if(args[1]="Myfont")
 {
-printFormat  = new LetterFormat();
+  printFormat  = new LetterFormat();
+} else {
+  printFormat  = new CharFormat();
+  printFormat.setPrintCharacter(args[1]);
 }
 for(i=1;i<9;i++)
 {
