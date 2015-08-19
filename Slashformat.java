493320e354b6ba1b033acc2f984c6c291d39168d
lavanyakommana <lavanyak36@gmail.com>
2015-08-19 09:59:41 +0530

    Update Slashformat.java

--- Slashformat.java
+++ Slashformat.java
@@ -1,9 +1,9 @@
- public class Slashformat extends Letterfomat
+ public class Slashformat extends Letterprinter
 {
 
-public static string getrowC(char c, int i)
+public String getrowC(char c, int i)
 {
-string y="";
+String y="";
 switch(i)
 {
 case 1:y="   __  _  ";break;
@@ -18,28 +18,28 @@ case 8:y="            ";break;
 }
 return(y);
 } 
-public static string getrowA(char c, int i)
+public  String getrowA(char c, int i)
 {
-string y="";
+String y="";
 switch(i)
 {
-case 1:y="- - - - - - -":break;
+case 1:y="- - - - - - -";break;
 case 2:y="| | - - - - - ";break;
 case 3:y="| |          ";break;
 case 4:y="| |          ";break;
 case 5:y="| |          ";break;
 case 6:y="| |          ";break;
-case 7:y="| - - - - - -":break;
-case 8:y="|- - - - - - -":break;
+case 7:y="| - - - - - -";break;
+case 8:y="|- - - - - - -";break;
 }
 return(y);
 }
-public static String getrowH(char c,int i)
+public  String getrowH(char c,int i)
 {
 String y="";
 switch(i)
 {
-ase 1:y="*  *        *  *";break;
+case 1:y="*  *        *  *";break;
 case 2:y="*  *        *  *";break;
 case 3:y="*  *        *  *";break;
 case 4:y="*  *        *  *";break;
@@ -50,7 +50,7 @@ case 8:y="*  *        *  *";break;
 }
 return(y);
 }
-public static String getrowN(char c,int i)
+public  String getrowN(char c,int i)
 {
 String y="";
 switch(i)
